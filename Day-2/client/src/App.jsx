d497a324145f0db6279d6a6efa7c7b453532e49b
Shubham Mishra <shubhammishra6595@gmail.com>
2025-01-05 09:05:46 +0530

    Day-2Update

--- Day-2/client/src/App.jsx
+++ Day-2/client/src/App.jsx
@@ -8,22 +8,19 @@ function App() {
     axios.get("http://localhost:3000/shubham").then((res) => {
       console.log(res);
       setData(res.data);
-      console.log(data);
-  
     });
   }, []);
 
-  return <>
-  
-  <h1>First Name {data?.firstName}</h1>
-  <h1>Age {data?.age}</h1>
-  <h1>Grade {data?.grade}</h1>
-  <h1>Subject 1 {data?.subjects[0]}</h1>
-  <h1>Subject 2 {data?.subjects[1]}</h1>
-  <h1>Subject 3 {data?.subjects[2]}</h1>
-  
-  
-  </>;
+  return (
+    <>
+      <h1>First Name {data?.firstName}</h1>
+      <h1>Age {data?.age}</h1>
+      <h1>Grade {data?.grade}</h1>
+      <h1>Subject 1 {data?.subjects[0]}</h1>
+      <h1>Subject 2 {data?.subjects[1]}</h1>
+      <h1>Subject 3 {data?.subjects[2]}</h1>
+    </>
+  );
 }
 
 export default App;
